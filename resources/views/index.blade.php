1611e0d935cba16bec8270d10392476483b4c21e
sytnic <sytnic@list.ru>
2023-03-29 10:19:36 +0300

    1.13 Layout inheritance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="ru">
+@extends('layouts.base')
+@section('title', 'Главная')
 
-<head>
-    <meta charset="UTF-8">
-    <title>Объявления</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" 
-        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-</head>
+@section('main')
 
-<body>
-  <div class="container">
-    <h1 class="my-3 text-center">Объявления</h1>
     @if (count($bbs) > 0)
     <table class="table table-striped">
       <thead>
@@ -34,14 +26,6 @@
       @endforeach
       </tbody>
     </table>
-    @endif    
-  </div>
-
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" 
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" 
-    integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
-</html>
+    @endif  
+      
+@endsection('main')
