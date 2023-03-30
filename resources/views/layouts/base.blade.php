cbac3f941e595ab95ef55e28749e1ad9169f4c56
sytnic <sytnic@list.ru>
2023-03-30 11:15:08 +0300

    1.15. Static files

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,6 +6,7 @@
     <title>@yield('title') :: Объявления</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link href="/styles/main.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
